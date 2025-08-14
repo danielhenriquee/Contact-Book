6ea7bc889135e2927195f321eea47bf22b1bf18b
Daniel Henrique <138157786+danielhenriquee@users.noreply.github.com>
2025-08-14 13:15:31 -0300

    Update contactBook.h

--- contactBook.h
+++ contactBook.h
@@ -6,60 +6,60 @@
 #include <unistd.h>
 using namespace std;
 
-// Template que permite criar uma struct agenda genérica (no caso do trabalho, pessoal ou comercial) e com um tamanho máximo
-template <typename TIPO, int MAX> 
-struct agenda {
-    TIPO itens[MAX]; // Vetor de itens (no caso do trabalho, struct pessoal ou comercial)
-    int quantidade;
+// Template que permite criar uma struct contactBook genérica (no caso do trabalho, personal  ou business) e com um tamanho máximo
+template <typename T, int MAX> 
+struct contactBook {
+    T items[MAX]; // Vetor de items (no caso do trabalho, struct personal  ou business)
+    int counter;
 };
 
 // Struct de contatos pessoais
-struct pessoal { 
-    int ID, CPF, Celular;
-    string Nome;
+struct personal  { 
+    int ID, SSN, mobilePhone;
+    string name;
 };
 
 // Struct de contatos comerciais
-struct comercial { 
-    int ID, CNPJ, Tel_comercial;
-    string Nome_empresa;
+struct business { 
+    int ID, CNPJ, companyPhone;
+    string companyName;
 };
 
-// Função para inicializar a agenda
-template <typename TIPO, int MAX> 
-bool inicializa_agenda(agenda<TIPO, MAX> &ag) {
-    ag.quantidade = 0; // Inicializa a quantidade de contatos na agenda como zero
+// Função para inicializar a contactBook
+template <typename T, int MAX> 
+bool contactBook_boot(contactBook<T, MAX> &cb) {
+    cb.counter = 0; // Inicializa a counter de contatos na contactBook como zero
     return true;
 }
 
-// Função para inserir um contato na agenda
-template <typename TIPO, int MAX> 
-bool inserir(agenda<TIPO, MAX> &ag, TIPO dado) {
-    if (ag.quantidade < MAX) { // Verifica se tem espaço na agenda
-        ag.itens[ag.quantidade] = dado; // Insere o contato na próxima posição disponível na agenda
-        ag.quantidade++; // Aumenta a contagem de contatos na agenda
+// Função para contactBook_insert um contato na contactBook
+template <typename T, int MAX> 
+bool contactBook_insert(contactBook<T, MAX> &cb, T dado) {
+    if (cb.counter < MAX) { // Verifica se tem espaço na contactBook
+        cb.items[cb.counter] = dado; // Insere o contato na próxima posição disponível na contactBook
+        cb.counter++; // Aumenta a contcbem de contatos na contactBook
         cout << "\nContato inserido com sucesso!" << endl;
         return true;
     } else {
-        cout << "Impossível inserir um novo contato, sua agenda está cheia!" << endl;
+        cout << "Impossível contactBook_insert um novo contato, sua contactBook está cheia!" << endl;
         return false;
     }
 }
 
-// Função para remover um contato da agenda por ID
-template <typename TIPO, int MAX> 
-bool remover(agenda<TIPO, MAX> &ag, int ID) {
+// Função para contactBook_delete um contato da contactBook por ID
+template <typename T, int MAX> 
+bool contactBook_delete(contactBook<T, MAX> &cb, int ID) {
     int aux = -1; // Inicializa uma variável auxiliar para indicar se o contato foi encontrado
-    for (int i = 0; i < ag.quantidade or aux == i; i++) {
-        if (ID == ag.itens[i].ID) {
+    for (int i = 0; i < cb.counter or aux == i; i++) {
+        if (ID == cb.items[i].ID) {
             aux = i; // Se encontrado, armazena a posição do contato a ser removido
         }
     }
     if (aux != -1) {
-        for (int i = aux; i < ag.quantidade; i++) {
-            ag.itens[i] = ag.itens[i + 1]; // A partir da posição do contato a ser removido, todos os contatos recebem os dados do contato seguinte, a posição onde ficava o último contato vira lixa de memória
+        for (int i = aux; i < cb.counter; i++) {
+            cb.items[i] = cb.items[i + 1]; // A partir da posição do contato a ser removido, todos os contatos recebem os dados do contato seguinte, a posição onde ficava o último contato vira lixa de memória
         }
-        ag.quantidade--; // Reduz a quantidade de contatos na agenda
+        cb.counter--; // Reduz a counter de contatos na contactBook
         cout << "Contato removido com sucesso.\n\n";
         return true;
     } else {
@@ -68,19 +68,19 @@ bool remover(agenda<TIPO, MAX> &ag, int ID) {
     }
 }
 
-// Função que utiliza um bubblesort genérico para ordenar os contatos da agenda por nome
-template <typename TIPO, int MAX>
-void ordenar(agenda<TIPO, MAX> &ag) { 
+// Função que utiliza um bubblesort genérico para contactBook_sort os contatos da contactBook por name
+template <typename T, int MAX>
+void contactBook_sort(contactBook<T, MAX> &cb) { 
     int i, j, cond;
-    TIPO temp;
+    T temp;
     cond = 1; // Inicializa a variável "cond" como 1 para entrar no primeiro loop
-    for (i = ag.quantidade - 1; (i >= 1) && (cond == 1); i--) { // Percorre a agenda a partir do último elemento até o primeiro
+    for (i = cb.counter - 1; (i >= 1) && (cond == 1); i--) { // Percorre a contactBook a partir do último elemento até o primeiro
         cond = 0;
         for (j = 0; j < i; j++) { // Compara e, se necessário, troca elementos adjacentes
-            if ((ag.itens[j + 1] < ag.itens[j]) == true) {
-                temp = ag.itens[j];
-                ag.itens[j] = ag.itens[j + 1];
-                ag.itens[j + 1] = temp;
+            if ((cb.items[j + 1] < cb.items[j]) == true) {
+                temp = cb.items[j];
+                cb.items[j] = cb.items[j + 1];
+                cb.items[j + 1] = temp;
                 cond = 1; // Define "cond" como 1 para indicar que houve uma troca
             }
         }
