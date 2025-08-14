f41027173d8b475059c74dc8158ef841eb89d6e4
Daniel Henrique <138157786+danielhenriquee@users.noreply.github.com>
2025-08-14 17:26:47 -0300

    Update contactBook.h

--- contactBook.h
+++ contactBook.h
@@ -6,82 +6,82 @@
 #include <unistd.h>
 using namespace std;
 
-// Template que permite criar uma struct contactBook genérica (no caso do trabalho, personal  ou business) e com um tamanho máximo
+// Generic contactBook struct with a maximum size
 template <typename T, int MAX> 
 struct contactBook {
-    T items[MAX]; // Vetor de items (no caso do trabalho, struct personal  ou business)
+    T items[MAX]; // Contacts vector (structs)
     int counter;
 };
 
-// Struct de contatos pessoais
+// Personal contacts struct
 struct personal  { 
     int ID, SSN, mobilePhone;
     string name;
 };
 
-// Struct de contatos comerciais
+// Business contacts struct
 struct business { 
     int ID, CNPJ, companyPhone;
     string companyName;
 };
 
-// Função para inicializar a contactBook
+// Boot contactBook
 template <typename T, int MAX> 
 bool contactBook_boot(contactBook<T, MAX> &cb) {
-    cb.counter = 0; // Inicializa a counter de contatos na contactBook como zero
+    cb.counter = 0;
     return true;
 }
 
-// Função para contactBook_insert um contato na contactBook
+// Insert new contact
 template <typename T, int MAX> 
 bool contactBook_insert(contactBook<T, MAX> &cb, T dado) {
-    if (cb.counter < MAX) { // Verifica se tem espaço na contactBook
-        cb.items[cb.counter] = dado; // Insere o contato na próxima posição disponível na contactBook
-        cb.counter++; // Aumenta a contcbem de contatos na contactBook
-        cout << "\nContato inserido com sucesso!" << endl;
+    if (cb.counter < MAX) { // CHeck if contact book is not full
+        cb.items[cb.counter] = dado; // Insert
+        cb.counter++;
+        cout << "\nContact added successfully!" << endl;
         return true;
     } else {
-        cout << "Impossível contactBook_insert um novo contato, sua contactBook está cheia!" << endl;
+        cout << "Unable to add a new contact, your contact book is full!" << endl;
         return false;
     }
 }
 
-// Função para contactBook_delete um contato da contactBook por ID
+// Delete by ID a contact
 template <typename T, int MAX> 
 bool contactBook_delete(contactBook<T, MAX> &cb, int ID) {
-    int aux = -1; // Inicializa uma variável auxiliar para indicar se o contato foi encontrado
+    int aux = -1; // Auxiliary variable to detect if contact was found
     for (int i = 0; i < cb.counter or aux == i; i++) {
         if (ID == cb.items[i].ID) {
-            aux = i; // Se encontrado, armazena a posição do contato a ser removido
+            aux = i; // If found, saves contact position
         }
     }
     if (aux != -1) {
         for (int i = aux; i < cb.counter; i++) {
-            cb.items[i] = cb.items[i + 1]; // A partir da posição do contato a ser removido, todos os contatos recebem os dados do contato seguinte, a posição onde ficava o último contato vira lixa de memória
+            cb.items[i] = cb.items[i + 1]; // Swift all contacts to the left
         }
-        cb.counter--; // Reduz a counter de contatos na contactBook
-        cout << "Contato removido com sucesso.\n\n";
+        cb.counter--; 
+        cout << "Contact successfully deleted.\n\n";
         return true;
     } else {
-        cout << "Contato não encontrado.\n\n";
+        cout << "Contact not found.\n\n";
         return false;
     }
 }
 
-// Função que utiliza um bubblesort genérico para contactBook_sort os contatos da contactBook por name
+// Generic bubblesort to sort contacts by name
 template <typename T, int MAX>
 void contactBook_sort(contactBook<T, MAX> &cb) { 
     int i, j, cond;
     T temp;
-    cond = 1; // Inicializa a variável "cond" como 1 para entrar no primeiro loop
-    for (i = cb.counter - 1; (i >= 1) && (cond == 1); i--) { // Percorre a contactBook a partir do último elemento até o primeiro
+    cond = 1;
+    for (i = cb.counter - 1; (i >= 1) && (cond == 1); i--) {
         cond = 0;
-        for (j = 0; j < i; j++) { // Compara e, se necessário, troca elementos adjacentes
+        for (j = 0; j < i; j++) {
             if ((cb.items[j + 1] < cb.items[j]) == true) {
                 temp = cb.items[j];
                 cb.items[j] = cb.items[j + 1];
                 cb.items[j + 1] = temp;
-                cond = 1; // Define "cond" como 1 para indicar que houve uma troca
+                cond = 1;
             }
         }
     }
